9a038b2e2040e4d08193575c4c9e6589029541f1
roshi2602 <roshidubey2602@gmail.com>
2021-06-24 14:44:19 +0530

    first commit

---
+++ BoxingNUnboxing-CSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Converting a value type to a reference type is called called boxing 
+//int x =10;  //value type
+//object o =x; //conversion of value type to reference type
+//here o is refernce type
+namespace BoxingNUnboxing_CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 10;  //value type
+            object o = i; //conversion to reference type
+
+            //now change value of i  
+            i = 20;
+            Console.WriteLine(@"{0}", i);
+            Console.WriteLine(@"{0}", o);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Unboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//unboxing is process of converting reference type to   value type
+
+namespace Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //assign value to int type
+            int i = 100;
+            //boxing it
+            object o = i;
+
+            //unboxing it
+            int j = (int)o;
+
+
+            //display result
+            Console.WriteLine(o);
+            Console.WriteLine(j);
+            Console.ReadLine();
+
+        }
+    }
+}
